d9203dddf354c225e1a9c03db43e1aab52dd4365
waghchaurepooja <97495331+waghchaurepooja@users.noreply.github.com>
2022-03-21 11:37:40 +0530

    Update db.js

--- u4 c3 evalution/models/config/db.js
+++ u4 c3 evalution/models/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://NayanIngale:nayan123@test.wjg1r.mongodb.net/unit4c3"
+    "mongodb+srv://pooja:pooja123@test.wjg1r.mongodb.net/unit4c3"
   );
 };
